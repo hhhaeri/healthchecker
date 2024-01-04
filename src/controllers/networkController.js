a1d72b2b8d5053aec4bca8210a8dabf60e5419df
handj622 <handj622@gmail.com>
2024-01-04 15:53:18 +0900

    network API 업데이트

--- src/controllers/networkController.js
+++ src/controllers/networkController.js
@@ -1,123 +1,20 @@
-
 const dotenv = require('dotenv');
 const fs = require('fs').promises;
 const yaml = require('js-yaml');
-const { Client } = require('ssh2');
-const sshConfig = require('../configs/sshConfig');
+const networkModel = require('../models/networkModel')
 dotenv.config();
 
-/******************************************************************************
- *                              Network Controller
- ******************************************************************************/
 class NetworkController {
     networkCheck = async (req, res, next) => {
-
         const filePath = 'src/configs/check.yaml';
+        const data = await fs.readFile(filePath, 'utf8');
+        const parsedData = yaml.load(data);
+        const dataArray = Array.isArray(parsedData) ? parsedData : [parsedData];
+        
+        let dataList = await networkModel.check(dataArray[0].network);
 
-        try {
-            const data = await fs.readFile(filePath, 'utf8');
-            const parsedData = yaml.load(data);
-            const dataArray = Array.isArray(parsedData) ? parsedData : [parsedData];
-
-            const network = dataArray[0].network;
-
-            const networkList = network.map(item => {
-                const command = item.kind === 'host' ? `ping -w 1 ${item.ip}` : `nc -zvw1 ${item.ip} ${item.port}`;
-                return {
-                    name: item.name,
-                    kind: item.kind,
-                    ip: item.ip,
-                    command: command,
-                    ...(item.kind === 'port' && { protocol: item.protocol }),
-                    ...(item.kind === 'port' && { port: item.port }),
-                };
-            });
-
-            const result = [];
-
-            const conn = new Client();
-
-            conn.on('ready', () => {
-                executeCommands(networkList);
-            });
-
-            function executeCommands(commandObjList) {
-                if (!commandObjList || commandObjList.length === 0) {
-                    conn.end(); // Close the connection
-                    res.send(result);
-                    return;
-                }
-
-                const commandObj = commandObjList.shift();
-                const command = commandObj.command;
-
-                conn.exec(command, (err, stream) => {
-                    if (err) {
-                        console.error(`Error executing command: ${command}\n${err}`);
-                        res.status(500).send('Internal Server Error');
-                        return;
-                    }
-
-                    let output = '';
-                    let errorOutput = '';
-
-                    stream.on('data', data => {
-                        output += data.toString();
-                    });
-
-                    stream.stderr.on('data', data => {
-                        errorOutput += data.toString();
-                    });
-
-                    stream.on('close', (code, signal) => {
-
-                        if (commandObj.kind === "host") {
-                            if (output.includes(", 0% packet loss,")) {
-                                result.push({
-                                    name: commandObj.name,
-                                    status: 'ok',
-                                    ip: commandObj.ip,
-                                });
-                            } else if (output.includes("100% packet loss")) {
-                                result.push({
-                                    name: commandObj.name,
-                                    status: 'error',
-                                    ip: commandObj.ip,
-                                });
-                            }
-                        } else if (commandObj.kind === "port") {
-                            if (errorOutput.includes("succeeded!")) {
-                                result.push({
-                                    name: commandObj.name,
-                                    status: 'ok',
-                                    ip: commandObj.ip,
-                                    port: commandObj.port,
-                                });
-                            } else if (errorOutput.includes("failed:") || errorOutput.includes("timed out:")) {
-                                result.push({
-                                    name: commandObj.name,
-                                    status: 'error',
-                                    ip: commandObj.ip,
-                                    port: commandObj.port,
-                                });
-                            }
-                        }
-
-                        executeCommands(commandObjList);
-                    });
-                });
-            }
-
-            conn.connect(sshConfig);
-
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Internal Server Error');
-        }
+        res.send(dataList);
     };
 }
 
-/******************************************************************************
- *                               Export
- ******************************************************************************/
 module.exports = new NetworkController;
--- src/models/networkModel.js
+++ src/models/networkModel.js
@@ -1,6 +1,37 @@
 const axios = require('axios');
+const ping = require('ping');
+const isReachable = require('is-reachable');
 
 class NetworkModel {
+    check = async (networkList) => {
+        // console.log(networkList)
+        const results = await Promise.all(
+            networkList.map((list) => {
+                return new Promise(async (resolve, reject) => {
+                    let template = {
+                        name: list.name,
+                        status: "",
+                        ip: list.ip
+                    };
+                    if(list.port === undefined){
+                        ping.promise.probe(list.ip)
+                        .then((res) => {
+                            res.alive === true ? template.status = "ok" : template.status = "error";
+                            resolve(template)
+                        });
+                    } 
+                    else{
+                        template.port = list.port;
+                        await isReachable(list.ip+":"+list.port) === true ? template.status = "ok" : template.status = "error";        
+                        resolve(template)
+                    }
+                })
+            })
+        );
+
+        return results
+    }
+
     mailcheck = async () => {
         try {
             const response = await axios.get('http://localhost:3000/api/v1/network/');
